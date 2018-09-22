7f009e62fed80b91c6ad93a4e1a557b327b69458
zyhang <i@zyhang.com>
2018-09-23 00:13:00 +0800

    💥【add】auto proguard & update gradle/kotlin version

--- buildSrc/src/main/java/Dependencies.kt
+++ buildSrc/src/main/java/Dependencies.kt
@@ -1,27 +1,26 @@
-@file:Suppress("unused")
+@file:Suppress("unused", "SpellCheckingInspection")
 
 object Configs {
-    val compileSdkVersion = 27
-    val minSdkVersion = 16
-    val targetSdkVersion = 27
+    const val compileSdkVersion = 27
+    const val minSdkVersion = 16
 }
 
 object Versions {
-    val kotlin = "1.2.41"
-    val support = "27.1.1"
-    val rxjava = "2.1.13"
+    const val kotlin = "1.2.70"
+    const val support = "27.1.1"
+    const val rxjava = "2.1.13"
 }
 
 object Libs {
-    val junit = "junit:junit:4.12"
-    val runner = "com.android.support.test:runner:1.0.2"
-    val espresso_core = "com.android.support.test.espresso:espresso-core:3.0.2"
+    const val junit = "junit:junit:4.12"
+    const val runner = "com.android.support.test:runner:1.0.2"
+    const val espresso_core = "com.android.support.test.espresso:espresso-core:3.0.2"
 
-    val support_appcompat_v7 = "com.android.support:appcompat-v7:${Versions.support}"
+    const val support_appcompat_v7 = "com.android.support:appcompat-v7:${Versions.support}"
 
-    val kotlin_stdlib = "org.jetbrains.kotlin:kotlin-stdlib-jdk8:${Versions.kotlin}"
+    const val kotlin_stdlib = "org.jetbrains.kotlin:kotlin-stdlib-jdk8:${Versions.kotlin}"
 
-    val rxjava = "io.reactivex.rxjava2:rxjava:${Versions.rxjava}"
+    const val rxjava = "io.reactivex.rxjava2:rxjava:${Versions.rxjava}"
 
-    val constraint_layout = "com.android.support.constraint:constraint-layout:1.1.1"
+    const val constraint_layout = "com.android.support.constraint:constraint-layout:1.1.1"
 }
